041a39ef0a2e9a9d958f738e28057537b1a7952a
Aureliano <chun.hui.li@qq.com>
2024-12-04 22:18:12 +0800

    feat: log初稿

---
+++ src/log/my_logger.py
@@ -0,0 +1,28 @@
+import logging
+
+LOGGER = None
+
+
+def get_error_logger():
+    global LOGGER
+    if LOGGER is None:
+        LOGGER = setup_logger('my_logger', 'error.log')
+    return LOGGER
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Function setup as many loggers as you want"""
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+
